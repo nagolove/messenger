9ee6b7cb7a35f2ecf8d0840abaf2baa376204e14
nagolove <dekar.own@gmail.com>
2022-07-03 23:22:23 +0300

    generated file.

--- thread.lua
+++ thread.lua
@@ -6,17 +6,11 @@ local msg_addr = vars[1]
 
 print('thread msg_addr', msg_addr)
 
-local msg = require('messenger')
+local msg = require('messenger2')
+local chan = msg.new()
 
-msg.connect(msg_addr)
-
-if not msg.connect(msg_addr) then
-
-   print('Something wrong with "msg.connect()"')
-end
-
-print('connected.')
-
-
-local value = msg.pop()
-print('value', value)
+local v
+repeat
+   v = chan:pop()
+   print('value', v)
+until not v
